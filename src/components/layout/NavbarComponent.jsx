ee208ffa63b251771ebe2bd0258ee5ba916db1a0
Sam-Sokunsreypich <sokunsreypichsam@gmail.com>
2024-10-18 11:01:02 +0700

    tewelthpush

--- src/components/layout/NavbarComponent.jsx
+++ src/components/layout/NavbarComponent.jsx
@@ -64,7 +64,7 @@ export default function NavbarComponent() {
                 className={({ isActive }) =>
                   isActive
                     ? "block py-2 px-3 md:p-0 md:text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                    : "block py-2 px-3 md:p-0 md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    : "block py-2 px-3 md:p-0 md:text-xl text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 }
               >
                 Story
@@ -77,7 +77,7 @@ export default function NavbarComponent() {
                 className={({ isActive }) =>
                   isActive
                     ? "block py-2 px-3 md:p-0 md:text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                    : "block py-2 px-3 md:p-0 md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    : "block py-2 px-3 md:p-0 md:text-xl text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 }
               >
                 Character
@@ -89,7 +89,7 @@ export default function NavbarComponent() {
                 className={({ isActive }) =>
                   isActive
                     ? "block py-2 px-3 md:p-0 md:text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                    : "block py-2 px-3 md:p-0 md:text-xl hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    : "block py-2 px-3 md:p-0 md:text-xl text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 }
               >
                 User Interface
@@ -101,7 +101,7 @@ export default function NavbarComponent() {
                 className={({ isActive }) =>
                   isActive
                     ? "block py-2 px-3 md:p-0 md:text-xl text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                    : "block py-2 px-3 md:p-0 md:text-xl  hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    : "block py-2 px-3 md:p-0 md:text-xl text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 }
               >
                 AboutUs
